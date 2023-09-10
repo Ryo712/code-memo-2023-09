73a62403d6fe15bb265bd436302a3377c4758312
大城亮 <hiltoc.katsu@gmail.com>
2023-09-10 16:21:59 +0900

    成功したアカウント調整のメッセージとDoneボタン

---
+++ 10misc-reconcile.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-sheet
+    elevation="12"
+    max-width="600"
+    rounded="lg"
+    width="100%"
+    class="pa-4 text-center mx-auto"
+  >
+<!--<v-sheet>: Vuetifyのコンポーネントで、カードライクな外観を持つコンテナ要素です。以下の属性が指定されています。-->
+    <v-icon
+      class="mb-5"
+      color="success"
+      icon="mdi-check-circle"
+      size="112"
+    ></v-icon>
+<!--Vuetifyのアイコンコンポーネントで、色が成功を示す緑色に設定され、mdi-check-circle アイコンが表示されます。-->
+
+    <h2 class="text-h5 mb-6">You reconciled this account</h2>
+    <!--テキストのタイトルを表す見出し要素-->
+
+    <p class="mb-4 text-medium-emphasis text-body-2"> <!--メッセージの本文を表す段落要素-->
+      To see a report on this reconciliation, click <a href="#" class="text-decoration-none text-info">View reconciliation report.</a>
+
+      <br>
+<!-- <a href~>:テキスト内にリンクが含まれており、リンクテキストは「View reconciliation report.」です。リンクの色は青く、下線を持たないスタイルに設定されています。-->
+      Otherwise, you're done!
+    </p>
+
+    <v-divider class="mb-4"></v-divider>
+<!--区切り線を表示します。マージン(空白領域)を持ち、セクションを視覚的に区切ります。4ピクセルのスペースが生まれる。-->
+    <div class="text-end">
+      <v-btn
+        class="text-none"
+        color="success"
+        rounded
+        variant="flat"
+        width="90"
+      >
+        Done
+      </v-btn>
+    </div>
+  </v-sheet>
+</template>
+<!--成功したアカウント調整の通知メッセージを表示し、詳細情報へのリンクと「Done」ボタンを提供するシンプルなUIコンポーネントを定義している。-->
