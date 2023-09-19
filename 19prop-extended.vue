f5df2bf3408dc014d229910fb421bc8d76342753
大城亮 <hiltoc.katsu@gmail.com>
2023-09-19 22:59:30 +0900

    ツールバー内にタイトルとアイコンボタン設置

---
+++ 19prop-extended.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card
+    color="grey-lighten-4"
+    flat
+    height="200px"
+    rounded="0" 
+  >
+<!-- カードの角を丸めずに設定 -->
+    <v-toolbar extended>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+<!--これはVuetifyのアイコンボタンコンポーネントで、ツールバー内でよく使用されます。
+このコンポーネントは通常、メニューやサイドバーを開くトリガーとして使用されます。-->
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+<!-- ボタンを表示するためのv-btnコンポーネントを使用 -->
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
+</template>
