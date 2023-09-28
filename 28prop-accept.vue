a56017ff5a63cdd68e565d2ed823aa64a4a3fce7
大城亮 <hiltoc.katsu@gmail.com>
2023-09-28 20:19:19 +0900

    ファイルアップロードのユーザーインターフェース

---
+++ 28prop-accept.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-file-input
+    accept="image/*" 
+    label="File input"
+  ></v-file-input>
+</template>
+<!--accept:受け入れるファイルの種類を指定。ここでは image/* が指定。ここでは画像ファイルが指定されている-->
+<!--label:ファイル選択ボタンのラベルテキストを指定。"File input"というテキストが表示されます-->
