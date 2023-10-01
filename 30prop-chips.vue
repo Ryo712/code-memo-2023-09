9ca65fb1fe650784424a23f56a660aeaf5bcd89b
大城亮 <hiltoc.katsu@gmail.com>
2023-10-01 14:09:04 +0900

    File input

--- 30prop-chips.vue
+++ 30prop-chips.vue
@@ -5,6 +5,9 @@
       multiple
       label="File input w/ chips"
     ></v-file-input>
+<!--chips: ファイルの選択後、ファイル名が表示される「チップ」（小さな表示要素）を有効にします。選択したファイルが表示されます。
+    multiple: 複数のファイルを選択できるようにします。
+    ラベルは "File input w/ chips"です。-->
     <v-file-input
       small-chips
       multiple
